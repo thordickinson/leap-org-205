c2b158bfe979c6ebd52227e6ca98eba81a5563a5
Dickinson Arismendy <dickinson@leapfin.com>
2022-06-11 21:05:58 -0700

    feat: using core api

--- mapping.js
+++ mapping.js
@@ -1,5 +1,11 @@
 async function mapToCoreEntities(graph, context) {
     console.log("Hello from  mapping")
+    const stripeInvoice = graph.findNodeByType("stripe-invoice")
+    const invoice = context.createInvoice(stripeInvoice).withData({
+        "customFields": {
+        }
+    })
+    return [invoice]
 }
 
 module.exports = mapToCoreEntities
