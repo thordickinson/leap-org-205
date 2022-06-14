0cabafb237ecc300d93d88225bb43d535dd7cbd8
Dickinson Arismendy <dickinson@leapfin.com>
2022-06-13 17:26:51 -0700

    fix:  accountig entry call

---
+++ src/indexer.js
@@ -0,0 +1,6 @@
+async function extractIndexedData(coreObject, indexingContext) {
+    //Push data to ES
+    //What data should we index?
+}
+
+module.exports = extractIndexedData
--- src/rules.js
+++ src/rules.js
@@ -2,7 +2,7 @@ async function applyRules(coreGraph, ruleContext) {
     const invoice = coreGraph.findNodeByLeapfinDataType("leapfinInvoice")
     ruleContext.logI("Processing invoice")
     const account = ruleContext.getOrgConfiguration("AR_ACCOUNT_NAME")
-    invoice.withAccountEntry({
+    invoice.withAccountingEntry({
         date: "2022-05-04",
         "entries": {
             "amount": 222,
