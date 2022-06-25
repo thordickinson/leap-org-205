cc1850c3f6ebecdb921fa5e97976b3cf9cbf11cd
Dickinson Arismendy <dickinson@leapfin.com>
2022-06-25 09:18:29 -0700

    feat: testing

--- src/mapper.js
+++ src/mapper.js
@@ -1,7 +1,7 @@
 function mapToCore(graph) {
     console.log("Hello from  mapping")
     const { data, metadata } = graph
-    const invoice = { id: metadata.id, anotherProperty: anotherInternalFunction() }
+    const invoice = { id: metadata.id, internalCall: anotherInternalFunction() }
     return [invoice]
 }
 
